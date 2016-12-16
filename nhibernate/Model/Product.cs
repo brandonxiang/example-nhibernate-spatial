8088acac077bf11e741988513532b80d5d92b837
brandonxiang <1542453460@qq.com>
2016-12-16 18:40:01 +0800

    init

---
+++ nhibernate/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nhibernate.Model
+{
+    public class Product
+    {
+        public virtual int id { get; set; }
+        public virtual string name { get; set; }
+        public virtual string code { get; set; }
+    }
+}
